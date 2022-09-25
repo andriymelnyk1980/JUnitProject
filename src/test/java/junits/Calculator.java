a7b84b051d69447ab4541df427f8c69758ac6aad
andriymelnyk <andriymelnyk1980@gmail.com>
2022-09-25 02:47:01 -0400

    commit1

---
+++ src/test/java/junits/Calculator.java
@@ -0,0 +1,16 @@
+package junits;
+
+public class Calculator {
+
+	public static int Add(int a, int b){
+		
+		return a + b;
+	}
+	public static int Sub(int a, int b) {
+		
+		return a - b;
+		
+	
+	}
+
+}
